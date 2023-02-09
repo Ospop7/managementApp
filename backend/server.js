f7556e0b19724c0b3cc0c56f8aadd28c4751f51b
Cbreaker <cbreaker@pop-os.localdomain>
2023-02-09 15:47:37 +0545

    working with routes and user model

--- backend/server.js
+++ backend/server.js
@@ -2,30 +2,30 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose  = require("mongoose");
-mongoose.set('strictQuery', false);
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Home page");
-
-})
-
-
-
+});
 
 // connect to DB and start server
 const port = process.env.PORT || 8087;
 
-mongoose.connect(process.env.MONGO_STRING)
-.then(()=>{
-  app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+mongoose
+  .connect(process.env.MONGO_STRING)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
   })
-}).catch((err) => {
-  console.error(err);
-})
-   
-
+  .catch((err) => {
+    console.error(err);
+  });
