44d56d406020fcff3bf528a80cae5b264528a440
Cbreaker <cbreaker@pop-os.localdomain>
2023-02-09 15:39:25 +0545

    create a server and connect mongoDB

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose  = require("mongoose");
+mongoose.set('strictQuery', false);
+const app = express();
+
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home page");
+
+})
+
+
+
+
+// connect to DB and start server
+const port = process.env.PORT || 8087;
+
+mongoose.connect(process.env.MONGO_STRING)
+.then(()=>{
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  })
+}).catch((err) => {
+  console.error(err);
+})
+   
+
