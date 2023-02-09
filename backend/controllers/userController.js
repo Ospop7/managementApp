ccc6c04035645226757e0a02827bc36bf5915815
Cbreaker <cbreaker@pop-os.localdomain>
2023-02-09 17:32:29 +0545

    validate the register user:

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,51 @@
-const registerUser =  (req, res) => {
-  if (!req.body.email) {
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+// Register User
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //   Validation
+  if (!name || !email || !password) {
     res.status(400);
-    throw new Error("Please add an email address");
+    throw new Error("please fill in all required fields");
   }
-  res.send("Register user");
-};
-const loginUser = async (req, res) => {
-  res.send("login user");
-};
-const logout = async (req, res) => {
-  res.send("Register user");
-};
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("password must be at least 6 characters");
+  }
+
+  //    Check if user email already exists
+  // When you interact with database you need model
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email has already been registered");
+  }
+  // Create new user if user don't exit
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {});
+const logout = asyncHandler(async (req, res) => {});
 module.exports = {
   registerUser,
   loginUser,
