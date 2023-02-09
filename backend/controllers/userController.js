ea1ddf73d04cae3377d7ec717887d4f903752a56
Cbreaker <cbreaker@pop-os.localdomain>
2023-02-09 18:20:38 +0545

    eccrypt password in userModel before saving in DB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,16 +25,13 @@ const registerUser = asyncHandler(async (req, res) => {
   }
   //   we gothe hass the password before create the new user
 
-  // encrypt password before saving to Db
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
+  
   // Create new user if user don't exit
   const user = await User.create({
     name,
     email,
-    password: hashedPassword,
+    password,
   });
   if (user) {
     const { _id, name, email, photo, phone, bio } = user;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -41,6 +42,17 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// encrypt password before saving to Db
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+});
 
 const User = mongoose.model("User", userSchema);
 
